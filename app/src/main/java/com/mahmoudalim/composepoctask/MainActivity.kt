b5856b718d24385f0b246867ab025818e8443faf
Mahmoud Hussein <mahmoud.hussein@gen-c.com>
2022-03-03 22:16:33 +0200

    implement splash screen
    Add splash.xml theme, add drawable logo and installSplash in app module MainActivity.kt

--- app/src/main/java/com/mahmoudalim/composepoctask/MainActivity.kt
+++ app/src/main/java/com/mahmoudalim/composepoctask/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.mahmoudalim.composepoctask.ui.theme.ComposePocTaskTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             ComposePocTaskTheme {
                 // A surface container using the 'background' color from the theme
