08d2484639f916dce35f3829f7f19e7ecd63e445
Mahmoud Hussein <mahmoud.hussein@gen-c.com>
2022-03-04 16:47:11 +0200

    optimize imports

--- app/src/main/java/com/mahmoudalim/composepoctask/MainActivity.kt
+++ app/src/main/java/com/mahmoudalim/composepoctask/MainActivity.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.mahmoudalim.composepoctask.ui.theme.ComposePocTaskTheme
 import com.mahmoudalim.presentation.compose.ShimmerSkeletonView
-import com.mahmoudalim.presentation.compose.shimmerAnimation
-import com.mahmoudalim.presentation.theme.color.AppColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
