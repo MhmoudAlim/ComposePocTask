1b82f51183fb30d863ea76f34a3841aed2b796ea
Mahmoud Hussein <mahmoud.hussein@gen-c.com>
2022-03-04 16:08:47 +0200

    disable App dark theme in Theme.kt

--- app/src/main/java/com/mahmoudalim/composepoctask/ui/theme/Theme.kt
+++ app/src/main/java/com/mahmoudalim/composepoctask/ui/theme/Theme.kt
@@ -1,16 +1,9 @@
 package com.mahmoudalim.composepoctask.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
@@ -29,17 +22,12 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun ComposePocTaskTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+//    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
 
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
