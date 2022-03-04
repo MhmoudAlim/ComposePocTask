e02d9ef18c8370c0eec944d7c33411a81000c42c
Mahmoud Hussein <mahmoud.hussein@gen-c.com>
2022-03-04 20:33:32 +0200

    add navControllee and NAvHost to MainActivity.kt to navigate between AppScafoold content composable screens

--- app/src/main/java/com/mahmoudalim/composepoctask/MainActivity.kt
+++ app/src/main/java/com/mahmoudalim/composepoctask/MainActivity.kt
@@ -3,26 +3,46 @@ package com.mahmoudalim.composepoctask
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Surface
-import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mahmoudalim.composepoctask.navigation.navigate
+import com.mahmoudalim.composepoctask.ui.screens.HomeScreen
+import com.mahmoudalim.composepoctask.ui.screens.SkeletonScreen
 import com.mahmoudalim.composepoctask.ui.theme.ComposePocTaskTheme
+import com.mahmoudalim.core.navigation.Route
 import com.mahmoudalim.presentation.compose.AppScaffoldHome
-import com.mahmoudalim.presentation.compose.ShimmerSkeletonView
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContent {
             ComposePocTaskTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = Color.White) {
-                    AppScaffoldHome() {
-                        ShimmerSkeletonView()
+                val navController = rememberNavController()
+                AppScaffoldHome {
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.SKELETON
+                    ) {
+                        composable(Route.SKELETON) {
+                            SkeletonScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.HOME) {
+                            HomeScreen()
+                        }
                     }
                 }
             }
         }
     }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
 }
+
