9ff3a0602430ae3808db77e5267d9691586be118
Mahmoud Hussein <mahmoud.hussein@gen-c.com>
2022-03-04 14:21:19 +0200

    create JsonFileParser.kt to parse json file to jsonString

---
+++ data/src/main/java/com/mahmoudalim/data/util/JsonFileParser.kt
@@ -0,0 +1,32 @@
+package com.mahmoudalim.data.util
+
+import android.content.Context
+import java.io.IOException
+import java.io.InputStream
+import java.nio.charset.Charset
+
+/**
+ * Created by Mahmoud Alim on 04/03/2022.
+ */
+
+object JsonFileParser {
+
+    private const val CHARSET = "UTF-8"
+    const val JSON_FILE_NAME = "mockResponse.json"
+
+    operator fun invoke(context: Context, fileName: String = JSON_FILE_NAME): String? {
+
+        val jsonString: String = try {
+            val stream: InputStream = context.assets.open(fileName)
+            val size: Int = stream.available()
+            val buffer = ByteArray(size)
+            stream.read(buffer)
+            stream.close()
+            String(buffer, Charset.forName(CHARSET))
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return null
+        }
+        return jsonString
+    }
+}
