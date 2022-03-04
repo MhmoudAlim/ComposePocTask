4139359a8947307a5f75cda7e7ece7c737080ddb
Mahmoud Hussein <mahmoud.hussein@gen-c.com>
2022-03-04 20:24:41 +0200

    create Route object in core module to define all app routes to be used in navigation

---
+++ core/src/main/java/com/mahmoudalim/core/navigation/Route.kt
@@ -0,0 +1,10 @@
+package com.mahmoudalim.core.navigation
+
+/**
+ * Created by Mahmoud Alim on 04/03/2022.
+ */
+
+object Route {
+    const val SKELETON = "skeleton"
+    const val HOME = "home"
+}
