d65cb503ee739bf713dbfd0bad7810c4b520e607
Mahmoud Hussein <mahmoud.hussein@gen-c.com>
2022-03-06 00:27:41 +0200

    Add ViewMenuButton UI and refactor PostFooterView.kt, add needed assets

--- app/src/main/java/com/mahmoudalim/composepoctask/ui/compose/PostFooterView.kt
+++ app/src/main/java/com/mahmoudalim/composepoctask/ui/compose/PostFooterView.kt
@@ -2,19 +2,27 @@ package com.mahmoudalim.composepoctask.ui.compose
 
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.mahmoudalim.composepoctask.R
 import com.mahmoudalim.data.enums.AuthorType
 import com.mahmoudalim.data.models.ResponseItem
+import com.mahmoudalim.presentation.compose.AppGradientButton
 import com.mahmoudalim.presentation.compose.AppProfileImage
 import com.mahmoudalim.presentation.theme.Montserrat
 import com.mahmoudalim.presentation.theme.color.AppColor
+import com.mahmoudalim.presentation.theme.color.AppColor.ButtonGradiantEndColor
+import com.mahmoudalim.presentation.theme.color.AppColor.ButtonGradiantStartColor
+import com.mahmoudalim.presentation.theme.color.AppColor.ButtonTextColor
 
 /**
  * Created by Mahmoud Alim on 05/03/2022.
@@ -60,15 +68,34 @@ fun PostFooterDescriptionView(post: ResponseItem) {
                 fontWeight = FontWeight.Light
             )
         }
-
-
     }
     if (post.post.author.authorType == AuthorType.RESTAURANT) {
-        Button(onClick = { /*TODO*/ }) {
-
-        }
+        ViewMenuButton()
     }
+}
 
+
+@Composable
+private fun ViewMenuButton() {
+    val gradientBrush = Brush.linearGradient(
+        colors = listOf(
+            ButtonGradiantStartColor.copy(.35f),
+            ButtonGradiantEndColor.copy(.35f)
+        ),
+        start = Offset(0f, Float.POSITIVE_INFINITY),
+        end = Offset(Float.POSITIVE_INFINITY, 0f)
+    )
+
+    AppGradientButton(gradientBrush) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(R.string.view_menu),
+            color = ButtonTextColor,
+            textAlign = TextAlign.Center,
+            fontSize = 14.sp,
+            fontFamily = Montserrat
+        )
+    }
 }
 
 
--- presentation/src/main/java/com/mahmoudalim/presentation/theme/color/AppColor.kt
+++ presentation/src/main/java/com/mahmoudalim/presentation/theme/color/AppColor.kt
@@ -7,4 +7,7 @@ object AppColor {
     val AppGrayBGColor = "#E0E0E0".hexColor()
     val AppPrimaryFontColor = "#363636".hexColor()
     val DividerColor = "#979797".hexColor()
+    val ButtonTextColor = "#8A50DE".hexColor()
+    val ButtonGradiantStartColor = "#406AE7".hexColor()
+    val ButtonGradiantEndColor = "#C63AD6".hexColor()
 }
