88d6ed15f8d69af00267337557a010b25b567aaf
Mahmoud Hussein <mahmoud.hussein@gen-c.com>
2022-03-05 21:28:25 +0200

    add DateTime Object to buildSrc

---
+++ buildSrc/src/main/java/DateTime.kt
@@ -0,0 +1,8 @@
+/**
+ * Created by Mahmoud Alim on 05/03/2022.
+ */
+
+object DateTime {
+    private const val threeTenVersion = "1.2.2"
+    const val threeTen = "com.jakewharton.threetenabp:threetenabp:${threeTenVersion}"
+}
